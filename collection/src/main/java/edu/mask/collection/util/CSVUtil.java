861cd7b679017bb8fcec056f1a998a769ba8f01f
maskleo <masque@aliyun.com>
2018-08-03 14:17:49 +0800

    add csv append

--- collection/src/main/java/edu/mask/collection/util/CSVUtil.java
+++ collection/src/main/java/edu/mask/collection/util/CSVUtil.java
@@ -3,6 +3,9 @@ package edu.mask.collection.util;
 import com.csvreader.CsvReader;
 import com.csvreader.CsvWriter;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,7 +23,7 @@ public final class CSVUtil {
     public static void main(String[] args) throws Exception {
         System.out.println(CSVUtil.readHead(System.getProperty("user.dir") + "/data/2018-08-02.csv"));
         //[600359, 603988, 002400, 000301, 601518, 600653, 002761, 600070, 002659]
-        CSVUtil.writerValue(System.getProperty("user.dir") + "/data/2018-08-02.csv", new String[]{"600359", "603988", "002400", "000301", "601518", "600653", "002761", "600070", "002659"});
+        CSVUtil.appendValue(System.getProperty("user.dir") + "/data/2018-08-02.csv", new String[]{"600359", "603988", "002400", "000301", "601518", "600653", "002761", "600070", "002659"});
     }
 
     public static void writerValue(String path, String[] contents) throws Exception {
@@ -28,6 +31,15 @@ public final class CSVUtil {
         csvWriter.writeRecord(contents);
         csvWriter.writeRecord(contents);
         csvWriter.close();
+    }
+
+    public static void appendValue(String path, String[] contents) throws Exception {
+        File f = new File(path);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(f, true));
+        CsvWriter cwriter = new CsvWriter(writer, ',');
+        cwriter.writeRecord(contents, false);
+        cwriter.close();
+
     }
 
 }
